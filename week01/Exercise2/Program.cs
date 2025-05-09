831a4049117958b069be9b5bbf4fe98335fec560
TiagoWurmli <ti.wurmli@hotmail.com>
2025-05-08 21:56:58 -0300

    Did Ex 1 and 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage? ");
+        string gradeString = Console.ReadLine();
+        int gradePercentage = int.Parse(gradeString);
+        
+        if (gradePercentage >= 90) 
+        {
+            gradeString = "A";
+        }
+
+        else if (gradePercentage >= 80)
+        {
+            gradeString = "B";
+        }
+
+        else if (gradePercentage >= 70)
+        {
+            gradeString = "C";
+        }
+
+        else if (gradePercentage >= 60)
+        {
+            gradeString = "D";
+        }
+
+        else
+        {
+            gradeString = "F";
+        }
+
+        Console.WriteLine($"Your grade is {gradeString}.");
+
+        if (gradeString == "A" || gradeString == "B" || gradeString == "C")
+        {
+            Console.WriteLine("You Pass!");
+        }
+
+        else
+        {
+            Console.WriteLine("You fail, good luck next time, you will need it!");
+        }
+
     }
 }
